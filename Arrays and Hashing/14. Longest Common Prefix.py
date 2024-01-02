7582daf0cb703d9ac07e692a803f693777b80e07
Md.Hamid Hosen <mdhamidhosen8444@gmail.com>
2024-01-03 00:07:43 +0600

    14. Longest Common Prefix

---
+++ Arrays and Hashing/14. Longest Common Prefix.py
@@ -0,0 +1,17 @@
+from pyparsing import List
+
+
+class Solution:
+    def longestCommonPrefix(self, strs: List[str]) -> str:
+        res = ""
+        for i in range(len(strs[0])):
+            for s in strs:
+                if i == len(s) or s[i] != strs[0][i]:
+                    return res
+            res += strs[0][i]
+        return res
+
+
+object = Solution()
+print(object.longestCommonPrefix( strs = ["flower","flow","flight"]))
+        
